24557e587ca4ebfaf6b393dff3b2f1160aa72131
Grzegorz Wierzowiecki <nospam@nospam.com>
2021-05-13 00:36:06 +0200

    refactoring: moving stdin, stdout as parameters of run() (so later mocking for testing works)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+use std::io;
 
-pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+pub fn run<R: io::Read, W: io::Write> (_input: R, mut output: W) -> Result<(), Box<dyn std::error::Error>> {
+    writeln!(output, "Hello, world!")?;
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rusty_things_organizer_4fun_util::*;
+use std::io;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    run()
+    run(io::stdin(), io::stdout())
 }
