91e9a6d4007c3c4c8b7b768c96664df7d0f2763d
Grzegorz Wierzowiecki <nospam@nospam.com>
2021-05-13 01:01:47 +0200

    run() += path for file

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
 use std::io;
+use std::path::Path;
 
-pub fn run<R: io::Read, W: io::Write> (_input: R, mut output: W) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run<R, W, P>(
+    _input: R,
+    mut output: W,
+    things_db_path: P,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    R: io::Read,
+    W: io::Write,
+    P: AsRef<Path>,
+{
+    let _thingsdb = std::fs::File::open(things_db_path);
     writeln!(output, "Hello, world!")?;
     Ok(())
 }
 
 #[cfg(test)]
-mod tests {
-}
+mod tests {}
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use rusty_things_organizer_4fun_util::*;
 use std::io;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    run(io::stdin(), io::stdout())
+    let things_db_path = "./thingsdb.csv";
+    run(io::stdin(), io::stdout(), things_db_path)
 }
